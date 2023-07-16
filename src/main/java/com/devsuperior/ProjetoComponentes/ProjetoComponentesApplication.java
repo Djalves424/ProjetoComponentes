763d9b0685300d00f4bc369d99b65d6e7fdb6b23
djalves424 <djalves424@gmail.com>
2023-07-16 20:51:16 -0300

    Componentes e injeção de dependência

---
+++ src/main/java/com/devsuperior/ProjetoComponentes/ProjetoComponentesApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.ProjetoComponentes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoComponentesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoComponentesApplication.class, args);
+	}
+
+}
