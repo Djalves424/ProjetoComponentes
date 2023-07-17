313b62444fba166de3c4d9e57362dd77052ccb4d
djalves424 <djalves424@gmail.com>
2023-07-16 21:13:08 -0300

    Refaturado a classe ProjetoComponentesApplication

--- src/main/java/com/devsuperior/ProjetoComponentes/ProjetoComponentesApplication.java
+++ src/main/java/com/devsuperior/ProjetoComponentes/ProjetoComponentesApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjetoComponentesApplication implements CommandLineRunner {
-
+    
     public static void main(String[] args) {
         SpringApplication.run(ProjetoComponentesApplication.class, args);
 
